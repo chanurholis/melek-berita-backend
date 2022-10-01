08301d4f79190f5446d90424f82a5ae3ad1f4385
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-01 15:33:19 +0700

    feat: add data source lookups migration file

---
+++ database/migrations/2022_10_01_071550_create_data_source_lookups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataSourceLookupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_source_lookups', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->unique()->nullable(false);
+            $table->string('name')->nullable(false);
+            $table->string('description')->nullable();
+            $table->string('value')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_source_lookups');
+    }
+}
