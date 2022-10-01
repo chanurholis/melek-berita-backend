74470081d9167212c7765f6a1665386e79abb89e
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-01 15:33:56 +0700

    feat: add example - get kurs bca hari ini

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\DataSourceLookup;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', function () {
+    return DataSourceLookup::getKursBCAToday();
 });
