0f84f9ad49d9926d9726a94b3af557506becd1f7
Andre Silva <andrepgsilva@outlook.com>
2022-10-09 02:08:38 +0100

    Add BCA_KURS_HARI_INI missing constant

--- app/Constants.php
+++ app/Constants.php
@@ -6,4 +6,5 @@ class Constants
 {
     const BCA_EXCHANGE_RATE_TODAY = 'bca_exchange_rate_today';
     const WEST_JAVA_WEATHER_FORECAST = 'west_java_weather_forecast';
+    const BCA_KURS_HARI_INI = '';
 }
