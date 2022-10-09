24dff0c34147e9c36ef1f215614462dc5d1e373d
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-09 13:27:46 +0700

    improve: add example xml to json

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/', function () {
     return 'Backend Melek Berita V1';
 });
+
+Route::get('/debug', function () {
+    $dataSource = 'https://data.bmkg.go.id/DataMKG/TEWS/autogempa.xml';
+    $xmlString = file_get_contents($dataSource);
+    $xmlObject = simplexml_load_string($xmlString);
+    $json = json_encode($xmlObject);
+    $phpArray = json_decode($json, true);
+    return $phpArray;
+});
