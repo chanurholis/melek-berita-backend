6ee7984f72ad4fa17c711568bee71994378b3b9f
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-21 22:09:04 +0700

    feat: add constant world cup groups

--- app/Constants.php
+++ app/Constants.php
@@ -6,5 +6,14 @@ class Constants
 {
     const BCA_EXCHANGE_RATE_TODAY = 'bca_exchange_rate_today';
     const WEST_JAVA_WEATHER_FORECAST = 'west_java_weather_forecast';
-    const BCA_KURS_HARI_INI = '';
+    
+    // World Cup Group
+    const WORLD_CUP_GROUP_A = 'world_cup_group_a';
+    const WORLD_CUP_GROUP_B = 'world_cup_group_b';
+    const WORLD_CUP_GROUP_C = 'world_cup_group_c';
+    const WORLD_CUP_GROUP_D = 'world_cup_group_d';
+    const WORLD_CUP_GROUP_E = 'world_cup_group_e';
+    const WORLD_CUP_GROUP_F = 'world_cup_group_f';
+    const WORLD_CUP_GROUP_G = 'world_cup_group_g';
+    const WORLD_CUP_GROUP_H = 'world_cup_group_h';
 }
